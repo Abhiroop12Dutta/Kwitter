b3fcb6712f0c4641c6ec0d099aa6b933b46f448a
Abhiroop12Dutta <91063460+Abhiroop12Dutta@users.noreply.github.com>
2022-11-12 18:51:31 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,24 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyCShPYLkao3et8ZQPLQHsjuVMCsNb-m6yM",
+      authDomain: "kwitterrooms-68414.firebaseapp.com",
+      databaseURL: "https://kwitterrooms-68414-default-rtdb.firebaseio.com",
+      projectId: "kwitterrooms-68414",
+      storageBucket: "kwitterrooms-68414.appspot.com",
+      messagingSenderId: "848784240980",
+      appId: "1:848784240980:web:325917cd8b84c9748cd8c1"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
